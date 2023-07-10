dc2dc88d5cc75bd218f9cb7e89ace8be8bdaffbf
Pikaplejer1 <pendykmarcel@gmail.com>
2023-07-11 01:51:57 +0200

    Added a base scenario in solver function

--- Project/Project.cpp
+++ Project/Project.cpp
@@ -3,8 +3,50 @@
 
 #include <iostream>
 
+
+
+bool isFinished = false;
+
+bool isCanditate(int row, int collumn, int checkedNumber, int sudoku[9][9])
+{
+    for (int i = 0; i < 9; i++)
+        if (sudoku[row][i] == checkedNumber) return false;
+    for (int i = 0; i < 9; i++)
+                if (sudoku[i][row] == checkedNumber) return false;
+    return true;
+}
+
+bool isEmpty(int sudoku)
+{
+    if (sudoku == 0);
+        return true;
+}
+
+void solver(int sudoku[9][9],int row, int collumn)
+{
+    int checkedNumber = 0;
+    if (isFinished == true) return;
+    while(!isEmpty(sudoku[row][collumn]))
+    {
+        if (collumn<9)
+        {
+            collumn++;
+        }
+        row++;
+        collumn = 0;
+    }
+    for (int i = 0; i < 9; i++)
+    {
+        checkedNumber = i;
+        if (isEmpty(sudoku[row][collumn]) && isCanditate(row, collumn,checkedNumber, sudoku));
+        sudoku[row][collumn] = checkedNumber;
+    }
+
+}
+
 int main()
 {
+    const int rows = 9, collumns = 9;
     int sudoku[9][9] =
     {
         {0,0,4,0,5,0,0,0,0},
@@ -17,21 +59,11 @@ int main()
         {0,0,9,1,8,2,0,0,3},
         {0,0,0,0,6,0,1,0,0}
     };
-    bool IsFinished = false;
-    bool IsCandidat[9][9];
-    while (!IsFinished)
-    {
-        for (int i = 0; i < 9; i++)
-            for (int j = 0; j < 9; j++)
-            {
-                if (sudoku[i][j] == 0 && IsCandidat[i][j] == true)
-                {
-                   
-                }
-
-            }
-    }
-
+    for(int i=0;i<9;i++)
+        for (int j = 0; j < 9; j++)
+        {
+            solver(sudoku, i, j);
+        }
 
 }
 
